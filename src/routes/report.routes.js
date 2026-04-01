74b18adc2aa955a858b34add2b74fc254f9bcdc7
Hoanganhthu1301 <hoanganhthu1261@gmail.com>
2026-04-01 22:00:51 +0700

    update report

--- src/routes/report.routes.js
+++ src/routes/report.routes.js
@@ -5,5 +5,23 @@ const reportController = require("../controllers/report.controller");
 const { verifyToken, isAdmin } = require("../middlewares/auth.middleware");
 
 router.get("/revenue", verifyToken, isAdmin, reportController.getRevenueReport);
+router.post(
+  "/revenue/save",
+  verifyToken,
+  isAdmin,
+  reportController.saveRevenueReport
+);
+router.get(
+  "/revenue/saved",
+  verifyToken,
+  isAdmin,
+  reportController.getSavedRevenueReports
+);
+router.get(
+  "/revenue/saved/:id",
+  verifyToken,
+  isAdmin,
+  reportController.getSavedRevenueDetail
+);
 
 module.exports = router;
